c9fc89b6cae9d0b84af28678df6e2ab6239a12ef
cassicakes <aguiar.cas@gmail.com>
2016-03-22 09:06:55 -0700

    basic angular routes, no styling

---
+++ js/controllers.js
@@ -0,0 +1,8 @@
+angular.module("DragQueenCtrls", []) //not using services so left empty
+
+.controller("DragCtrl", [
+  "$scope",
+  function($scope) {
+    console.log("controller Twerking!")
+  }
+])
