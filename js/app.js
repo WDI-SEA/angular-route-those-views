94fea9296034ee715d15053b63249f6ad6775207
Joe Marzullo <jmarzu13@gmail.com>
2016-07-25 15:46:59 -0700

    First 3 routes work'

---
+++ js/app.js
@@ -0,0 +1,28 @@
+var myApp = angular.module('myApp', ['ui.router']);
+console.log('in the app');
+
+myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider.state('main', {
+    url: '/',
+    templateUrl: 'views/main.html',
+    controller: 'MainCtrl'
+  })
+  .state('about', {
+    url: '/about',
+    templateUrl: 'views/about.html',
+    controller: 'AboutCtrl'
+  })
+  .state('contact', {
+    url: '/contact',
+    templateUrl: 'views/contact.html',
+    controller: 'ContactCtrl'
+  });
+
+}]);
+
+// myApp.controller('MainCtrl', ['$scope', function($scope) {
+//   console.log('in the main controller');
+// }]);
+
