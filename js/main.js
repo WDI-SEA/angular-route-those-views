4ac63ea4931f233acf910f8b80e70f011719bcdd
darkenvy <darkenvy6@gmail.com>
2016-07-25 21:22:06 -0700

    MVP

---
+++ js/main.js
@@ -0,0 +1,57 @@
+var app = angular.module('Portfolio', ['ui.router']);
+
+app.controller('MainCtrl', ['$scope', function($scope) {
+  // $scope.text = 'main page';
+  $scope.projects = {
+    '1': 'one',
+    '2': 'two',
+    '3': 'three',
+    '4': 'four',
+    '5': 'five',
+    '6': 'six',
+    '7': 'seven',
+    '8': 'eight',
+    '9': 'nine',
+    '10': 'ten'
+  }
+}])
+
+app.controller('ProjectsCtrl', ['$scope', function($scope) {
+  $scope.text = 'projects list page'
+}])
+
+app.controller('ContactCtrl', ['$scope', function($scope) {
+  $scope.text = 'contact page'
+}])
+
+app.controller('AboutCtrl', ['$scope', function($scope) {
+  $scope.text = 'about page'
+}])
+
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('main', {
+      url: '/',
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .state('projects', {
+      url: '/projects',
+      templateUrl: 'views/projects.html',
+      controller: 'ProjectsCtrl'
+    })
+    .state('contact', {
+      url: '/contact',
+      templateUrl: 'views/contact.html',
+      controller: 'ContactCtrl'
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: 'views/about.html',
+      controller: 'AboutCtrl'
+    })
+
+
+}])
