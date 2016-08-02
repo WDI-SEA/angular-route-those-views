02bd610fee480e270a464984c180a0d9b642dc7e
darkenvy <darkenvy6@gmail.com>
2016-08-02 16:07:13 -0700

    A lot added

--- js/main.js
+++ js/main.js
@@ -2,14 +2,30 @@ var app = angular.module('Portfolio', ['ui.router']);
 
 app.factory('projects', function() {
   return {
-    'tmp/1': {img: '200x150',title: 'Project 1'},
-    'tmp/2': {img: '200x150',title: 'Project 2'},
-    'tmp/3': {img: '200x150',title: 'Project 3'},
-    'tmp/4': {img: '200x150',title: 'Project 4'},
-    'tmp/5': {img: '200x150',title: 'Project 5'},
-    'tmp/6': {img: '200x150',title: 'Project 6'},
-    'tmp/7': {img: '200x150',title: 'Project 7'},
-    'tmp/8': {img: '200x150',title: 'Project 8'}
+    '1': {title: 'Dungeon Explorer',    cover: 'project/cover-dungeon.png',
+          img: ['homocide1.png','homocide2.png'],
+          desc: 'Lorem Ipsum'},
+    '2': {title: 'SquidLink',           cover: 'project/cover-squidlink.png',
+          img: [''],
+          desc: 'Lorem Ipsum'},
+    '3': {title: 'Game of Life',        cover: 'project/cover-gol.png',
+          img: [''],
+          desc: 'Lorem Ipsum'},
+    '4': {title: 'Homocides by Police', cover: 'project/cover-homocide.png',
+          img: [''],
+          desc: 'Lorem Ipsum'},
+    '5': {title: 'Project 5',           cover: 'project/tmp.png',
+          img: [''],
+          desc: 'Lorem Ipsum'},
+    '6': {title: 'Project 6',           cover: 'project/tmp.png',
+          img: [''],
+          desc: 'Lorem Ipsum'},
+    '7': {title: 'Project 7',           cover: 'project/tmp.png',
+          img: [''],
+          desc: 'Lorem Ipsum'},
+    '8': {title: 'Project 8',           cover: 'project/tmp.png',
+          img: [''],
+          desc: 'Lorem Ipsum'}
   }
 })
 
@@ -17,8 +33,9 @@ app.controller('MainCtrl', ['$scope', 'projects', function($scope, projects) {
   $scope.projects = projects;
 }])
 
-app.controller('ProjectsCtrl', ['$scope', function($scope) {
-  $scope.text = 'projects list page'
+app.controller('ProjectsCtrl', ['$scope', 'projects', '$stateParams',
+  function($scope, projects, $stateParams) {
+  $scope.project = projects[$stateParams.id];
 }])
 
 app.controller('ContactCtrl', ['$scope', function($scope) {
@@ -39,7 +56,7 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
       controller: 'MainCtrl'
     })
     .state('projects', {
-      url: '/projects',
+      url: '/projects/:id',
       templateUrl: 'views/projects.html',
       controller: 'ProjectsCtrl'
     })
