ff1f4225b28e2461c89d38ce680e5e5976c5bdf1
darkenvy <darkenvy6@gmail.com>
2016-08-03 13:38:42 -0700

    Added colors and styled text

--- js/main.js
+++ js/main.js
@@ -1,35 +1,52 @@
+// Front end, to scroll to top:
+// window.scrollTo(0,0)
+
 var app = angular.module('Portfolio', ['ui.router']);
 
 app.factory('projects', function() {
   return {
-    '1': {title: 'Dungeon Explorer',    cover: 'project/cover-dung.png',
+    '1': {title: 'About Me',
+          info: true,
+          infoColor: {backgroundColor: '#E8ECF1'},
+          // infoColor: {backgroundColor: '#E3DFC8'},
+          // infoColor: {backgroundColor: '#DAF4F5'},
+          infoTitle: 'about me'
+          },
+
+    '2': {title: 'Dungeon Explorer',    cover: 'project/cover-dung.png',
           img: ['dungeon1.png','dungeon2.png'],
           desc: 'Lorem Ipsum'},
 
-    '2': {title: 'SquidLink',           cover: 'project/cover-squidl.png',
+    '3': {title: 'SquidLink',           cover: 'project/cover-squidl.png',
           img: ['squidlink1.png', 'squidlink2.png', 'squidlink3.png', 'squidlink4.png',],
           desc: 'Lorem Ipsum'},
 
-    '3': {title: 'Game of Life',        cover: 'project/cover-life.png',
+    '4': {title: 'Game of Life',        cover: 'project/cover-life.png',
           img: ['gol1.png', 'gol2.png'],
           desc: 'Lorem Ipsum'},
 
-    '4': {title: 'Homocides by Police', cover: 'project/cover-cops.png',
+    '5': {title: 'Homocides by Police', cover: 'project/cover-cops.png',
           img: ['homocide1.png','homocide2.png'],
           desc: 'Lorem Ipsum'},
 
-    '5': {title: 'Project 5',           cover: 'project/tmp.png',
-          img: [''],
-          desc: 'Lorem Ipsum'},
-
-    '6': {title: 'Project 6',           cover: 'project/tmp.png',
-          img: [''],
-          desc: 'Lorem Ipsum'},
-
-    '7': {title: 'About Me',            cover: 'project/tmp.png',
+    '6': {title: 'Contact Me',
           info: true,
-          infoColor: {backgroundColor: '#e2da6c'}
-          }
+          // infoColor: {backgroundColor: '#bcc5d6'},
+          infoColor: {backgroundColor: '#cdd7ea'},
+          // infoColor: {backgroundColor: '#E8ECF1'},
+          // infoColor: {backgroundColor: '#ABCECF'},
+          infoTitle: 'Contact Me'
+          },
+
+    '7': {title: 'Resumé',
+          info: true,
+          // infoColor: {backgroundColor: '#B5CFD8'},
+          infoColor: {backgroundColor: '#F5F1DA'},
+          // infoColor: {backgroundColor: '#C4DCE0'},
+          infoTitle: 'Resumé'
+          },
+
+
   }
 })
 
