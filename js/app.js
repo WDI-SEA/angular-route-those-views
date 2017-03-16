01d87a1daec7d3b3e24754be1cd508472c80c1f1
krichdev <kgrichardson2@gmail.com>
2017-03-16 15:39:41 -0700

    added angular app

---
+++ js/app.js
@@ -0,0 +1,41 @@
+var app = angular.module('BieberApp', ['ui.router']);
+
+app.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.gifs = [];
+
+  var req = {
+        url: "http://api.giphy.com/v1/gifs/search?q=justin-bieber&api_key=dc6zaTOxFJmzC",
+      }
+
+  $http(req).then(function success(res){
+    $scope.gifs = res.data.data
+    }, function error(req){
+      console.log("There was an error");
+    })
+
+
+}]);
+
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+  .state('home',{
+    url: '/',
+    templateUrl: 'views/home.html',
+  })
+  .state('music',{
+    url: '/music',
+    templateUrl: 'views/music.html'
+  })
+  .state('gifs',{
+    url: '/gifs',
+    templateUrl: 'views/gifs.html',
+    controller: 'AppCtrl'
+  })
+  .state('about',{
+    url: '/about',
+    templateUrl: 'views/about.html'
+  })
+  //$locationProvider.html5Mode(false).hasPrefix('!');
+}]);
